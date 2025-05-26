2fa16b2a905b377e0d8e3f3060f1bd306948f258
boterop <98826652+boterop@users.noreply.github.com>
2025-05-26 14:51:17 -0500

    First commit

---
+++ src/main/java/com/boterop/xphealth/XPHealth.java
@@ -0,0 +1,11 @@
+package com.boterop.xpascend;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(XPAscend.MOD_ID)
+public class XPAscend {
+    public static final String MOD_ID = "xpascend";
+
+    public XPAscend() {
+    }
+}
