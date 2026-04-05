ccd637144b3f166a51e4a676985ce60b7727e702
boterop <98826652+boterop@users.noreply.github.com>
2026-04-05 01:57:23 -0500

    Attributes

--- src/main/java/io/boterop/xpascend/events/XPEventHandler.java
+++ src/main/java/io/boterop/xpascend/events/XPEventHandler.java
@@ -2,10 +2,16 @@ package io.boterop.xpascend.events;
 
 import net.minecraft.world.entity.ai.attributes.Attributes;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jline.utils.Log;
+
 import io.boterop.xpascend.XPAscend;
 import io.boterop.xpascend.utils.Difficulty;
 
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
@@ -28,15 +34,61 @@ public class XPEventHandler {
         int playerExp = player.experienceLevel;
         float difficulty = Difficulty.getDifficulty(player);
 
-        int hearts = (int) Math.floor(playerExp * difficulty);
+        double amount = Math.floor(playerExp * difficulty);
 
-        hearts = hearts < 0 ? 0 : hearts;
+        amount = amount < 0 ? 0 : amount;
 
-        Identifier attributeId = Identifier.fromNamespaceAndPath(XPAscend.MODID, "max_health");
-        AttributeModifier modifier = new AttributeModifier(attributeId, hearts, AttributeModifier.Operation.ADD_VALUE);
+        String attrs[] = {"max_health", "attack_damage", "movement_speed", "knockback_resistance", "attack_speed", "attack_knockback", "luck"};
 
         AttributeMap attributes = player.getAttributes();
+        for (int i = 0; i < attrs.length; i++) {
+            String attr = attrs[i];
+            Identifier id = Identifier.fromNamespaceAndPath(XPAscend.MODID, attr);
+            
+            AttributeModifier modifier = new AttributeModifier(id, fixedValue(attr, amount), AttributeModifier.Operation.ADD_VALUE);
+            
+            getInstance(attributes, attr).addOrReplacePermanentModifier(modifier);
+        }
+    }
 
-        attributes.getInstance(Attributes.MAX_HEALTH).addOrReplacePermanentModifier(modifier);
+    private static AttributeInstance getInstance(AttributeMap attributes, String attr) {
+        switch (attr) {
+            case "max_health":
+                return attributes.getInstance(Attributes.MAX_HEALTH);
+            case "attack_damage":
+                return attributes.getInstance(Attributes.ATTACK_DAMAGE);
+            case "movement_speed":
+                return attributes.getInstance(Attributes.MOVEMENT_SPEED);
+            case "knockback_resistance":
+                return attributes.getInstance(Attributes.KNOCKBACK_RESISTANCE);
+            case "attack_speed":
+                return attributes.getInstance(Attributes.ATTACK_SPEED);
+            case "attack_knockback":
+                return attributes.getInstance(Attributes.ATTACK_KNOCKBACK);
+            case "luck":
+                return attributes.getInstance(Attributes.LUCK);
+            default:
+                return null;
+        }
+    }
+
+    private static double fixedValue(String attr, double amount) {
+        Map<String, Double> maxValues = new HashMap<>();
+        maxValues.put("attack_knockback", 5d);
+        maxValues.put("knockback_resistance", 1d);
+        maxValues.put("movement_speed", 0.08d);
+
+        switch (attr) {
+            case "attack_knockback":
+                return Double.min(amount * 0.2, maxValues.get(attr));
+            case "knockback_resistance":
+                return Double.min(amount, maxValues.get(attr)) / 100;
+            case "movement_speed":
+                return Double.min(amount * 0.2, maxValues.get(attr)) / 100;
+            case "attack_speed":
+                return amount * 0.5;
+            default:
+                return amount;
+        }
     }
 }
